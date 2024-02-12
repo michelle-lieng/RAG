892974fc6951ce84747b6fa4c3665b388ad029a0
crunchyfriedchicken <118226757+crunchyfriedchicken@users.noreply.github.com>
2024-02-12 19:37:38 +1100

    changed text_to_docs by removing chunking - Documents only have page number and filename metadata

--- pdf_chunker.py
+++ pdf_chunker.py
@@ -1,9 +1,8 @@
 # Import libraries
 import re
 from io import BytesIO
-from typing import List
+from typing import List # use List as then we can be specific on which elements the list can contain, e.g. List(str)
 from langchain.docstore.document import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pypdf import PdfReader
 
 def parse_pdf(file: BytesIO, filename: str) -> tuple[List[str], str]:
@@ -17,7 +16,7 @@ def parse_pdf(file: BytesIO, filename: str) -> tuple[List[str], str]:
     """
     # Initialize the PDF reader for the provided file
     pdf = PdfReader(file)
-    output = []
+    cleaned_text = []
     
     # Loop through all the pages in the PDF
     for page in pdf.pages:
@@ -37,59 +36,46 @@ def parse_pdf(file: BytesIO, filename: str) -> tuple[List[str], str]:
         text = re.sub(r" Y ", r" Y", text)
         
         # Append the cleaned text to the output list.
-        output.append(text)
+        cleaned_text.append(text)
     
     # Return the list of cleaned texts and the filename.
-    return output, filename
+    return cleaned_text, filename
 
 # Trialing the parse_pdf function
 #pdf = PdfReader(r"data\insurance_terms_and_conditions.pdf")
 #text = pdf.pages[0].extract_text()
-#parse_pdf(r"data\insurance_terms_and_conditions.pdf", "insurance_terms_and_conditions")
+# parse_pdf(r"data\insurance_tncs.pdf", "insurance_tncs")
 
 def text_to_docs(text: List[str], filename: str) -> List[Document]:
     """
 
+    Note: the text variable is a list of cleaned text where each element is a page from a pdf
+
     This function is used to:
         - Take a list of text strings & file name
-        - Processes the text to create a list of chunked "Document" objects
-        - These objects each represent a smaller portion of the original text with associated metadata
-    
+        - Processes the pages to create a list of "Document" objects that contain metadata of page and file name
+        
     """
     # Ensure the input text is a list. If it's a string, convert it to a list.
     if isinstance(text, str):
         text = [text]
     
-    # Convert each text (from a page) to a Document object.
+    # Convert each text (from a page) to a Langchain Document object
     page_docs = [Document(page_content=page) for page in text]
     
     # Assign a page number to the metadata of each document.
     for i, doc in enumerate(page_docs):
-        doc.metadata["page"] = i + 1
+        doc.metadata["page"] = 1+i #as enumerate starts at 0
+        doc.metadata["filename"] = filename
 
-    doc_chunks = []
-    
-    # Split each page's text into smaller chunks and store them as separate documents.
-    for doc in page_docs:
-        # Initialize the text splitter with specific chunk sizes and delimiters.
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=4000,
-            separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
-            chunk_overlap=0,
-        )
-        
-        # Split the document's text into chunks.
-        chunks = text_splitter.split_text(doc.page_content)
-        
-        # Convert each chunk into a new document, storing its chunk number, page number, and source file name in its metadata.
-        for i, chunk in enumerate(chunks):
-            doc = Document(
-                page_content=chunk, metadata={"page": doc.metadata["page"], "chunk": i}
-            )
-            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
-            doc.metadata["filename"] = filename
-            doc_chunks.append(doc)
-    
-    # Return the list of chunked documents.
-    return doc_chunks
+    return page_docs
 
+# trying the functions
+"""
+cleaned_text, filename = parse_pdf("data\\qantas_points_tncs.pdf", "qantas_points_tncs")
+include_metadata = text_to_docs(cleaned_text,filename)
+
+for line in include_metadata:
+    print(line)
+    print("\n")
+"""
