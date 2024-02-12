4f10030a5be350d142ce3f5e2207ea3e8c12dff4
crunchyfriedchicken <118226757+crunchyfriedchicken@users.noreply.github.com>
2024-02-12 19:38:38 +1100

    simplified function

--- vectordb.py
+++ vectordb.py
@@ -1,5 +1,6 @@
 # Import libaries
 from io import BytesIO
+from typing import List
 from langchain_openai import OpenAIEmbeddings
 from pdf_chunker import parse_pdf, text_to_docs
 from langchain.vectorstores.faiss import FAISS
@@ -8,9 +9,9 @@ from langchain.vectorstores.faiss import FAISS
 with open('GPT_api_key.txt') as f:
     openai_api_key = f.read()
 
-def create_vectordb(pdf_files, pdf_names, openai_api_key):
+def create_vectordb(pdf_files: List[str], pdf_names: List[str], openai_api_key: str):
+
     """
-    
     This function aims to index PDF files. It accepts a list of PDF file objects and their names.
 
     The steps outlined include:
@@ -20,17 +21,12 @@ def create_vectordb(pdf_files, pdf_names, openai_api_key):
 
     Langchain's "FAISS.from_documents" method takes a lists of documents and the openapi key 
     and changes all the documents into embeddings and creates a faiss vector database and index.
-
     """
+    
     documents = []
 
-    for pdf_path, pdf_name in zip(pdf_files, pdf_names):
-
-        # open the PDF file in binary mode ('rb') and ensure its content is read as bytes-like object
-        with open(pdf_path, 'rb') as file: 
-            pdf_file = file.read()
-
-        text, filename = parse_pdf(BytesIO(pdf_file), pdf_name)
+    for pdf_file, pdf_name in zip(pdf_files, pdf_names):
+        text, filename = parse_pdf(pdf_file, pdf_name)
         documents = documents + text_to_docs(text, filename)
 
     # documents are the cleaned and chucked PDF data
@@ -38,16 +34,35 @@ def create_vectordb(pdf_files, pdf_names, openai_api_key):
     
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     db = FAISS.from_documents(documents, embeddings)
-
+    
     return db
 
-# Trying the function
 """
-db = create_vectordb(r"data\amex.pdf", "amex", openai_api_key)
+try function
+
+def read_pdf_files_from_folder(folder_path: str):
+    pdf_files = []
+    pdf_names = []
+    
+    for file in os.listdir(folder_path):
+        if file.endswith(".pdf"):
+            pdf_names.append(file.rstrip(".pdf"))
+            pdf_files.append(os.path.join("data", file))
+    return pdf_files, pdf_names
+
+pdf_files, pdf_names = read_pdf_files_from_folder("data")   
+
+#db = create_vectordb([r"data\insurance_tncs.pdf"], ["insurance_tncs"], openai_api_key)
+
+db = create_vectordb(pdf_files, pdf_names, openai_api_key)
 
 query = "Who is eligible for travel insurance?"
 docs = db.similarity_search(query, k=3) #get 3 chunks
 
-docs[1] # can slice chunks like this -> get the 2nd chunk
-print(docs[0].page_content)
+for line in docs:
+    print(line)
+    print("\n")
+
+#docs[1] # can slice chunks like this -> get the 2nd chunk
+#print(docs[0].page_content)
 """
