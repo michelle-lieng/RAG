7ba41d8c1e61f1b58e733fe3a396a5c95009e909
crunchyfriedchicken <118226757+crunchyfriedchicken@users.noreply.github.com>
2024-02-27 23:59:03 +1100

    experimenting with os and sys modules

---
+++ docs/paths.py
@@ -0,0 +1,12 @@
+import os
+
+# Get the current working directory
+cwd = os.getcwd()
+
+# Print the current working directory
+print("Current working directory:", cwd)
+
+# check for full path of python interpreter
+import sys
+
+print(sys.executable)
